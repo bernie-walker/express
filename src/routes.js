838762ddbdee53ab9dc979e4ea90187b4c535b94
priyanshu <priyanshu.pal@thoughtworks.com>
2020-07-23 19:43:41 +0530

    |#12|Shivam/Priyanshu| changed variable name to camelCase

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ app.get('/dashboard', (req, res) => {
   res.render('dashboard', {
     avatarUrl: 'https://avatars2.githubusercontent.com/u/58025838?s=460&v=4',
     username: 'palPriyanshu',
-    display_name: 'priyanshu',
+    displayName: 'priyanshu',
   });
 });
 
