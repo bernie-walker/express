2c50bf74cdddb0dcf176cb4169f2ff069e4697ba
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-07-24 16:29:25 +0530

    |#13|berny/thanya|added Stories and getStory with ID

--- src/expressData.js
+++ src/expressData.js
@@ -5,9 +5,18 @@ const generateGetNStoriesQuery = function (offset, count) {
   ON str.written_by = usr.id
   WHERE state='published'
   ORDER BY str.last_modified DESC
-  LIMIT ${offset},${count};
-  `;
+  LIMIT ${offset},${count};`;
 };
+const generateGetStoryQuery = function (storyID) {
+  return `SELECT usr.display_name as authorName,usr.avatar_url as avatar,
+  str.title,str.content,str.id as storyID,str.written_by as authorID,
+  date(str.last_modified) as lastModified
+  FROM stories AS str
+  JOIN users AS usr 
+  ON str.written_by = usr.id
+  WHERE state='published' AND str.id='${storyID}';`;
+};
+
 class Stories {
   constructor(db) {
     this.db = db;
@@ -17,6 +26,16 @@ class Stories {
     return new Promise((resolve) => {
       this.db.all(query, (err, rows) => {
         resolve(rows);
+      });
+    });
+  }
+
+  getStory(storyID) {
+    const query = generateGetStoryQuery(storyID);
+    return new Promise((resolve) => {
+      this.db.get(query, (err, row) => {
+        row.content = JSON.parse(row.content);
+        resolve(row);
       });
     });
   }
