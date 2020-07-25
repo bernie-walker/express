ffd838dbc1c083649a69ae746cd844a06a1400a3
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-07-25 23:16:15 +0530

    |#11|bernie/priyanshu|modified onclick function of logo on dashboard

--- public/js/header.js
+++ public/js/header.js
@@ -1,14 +1,22 @@
 const getElement = (selector) => document.querySelector(selector);
 
+const gotoDashboard = () => {
+  window.location = '/dashboard';
+};
+
+const attachGotoDashboardToLogo = function () {
+  Array.from(document.querySelectorAll('#logo  svg'), (el) =>
+    el.addEventListener('click', gotoDashboard)
+  );
+};
+
 const attachHeadListener = function () {
   const $profile = getElement('#profile');
   $profile.addEventListener('click', () => {
     getElement('.profile-modal').classList.toggle('hidden');
   });
 
-  logo.onclick = () => {
-    window.location = '/dashboard';
-  };
+  attachGotoDashboardToLogo();
 
   attachBodyListeners();
 };
