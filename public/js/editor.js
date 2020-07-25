9a8718ff8643bf56ca192025bb2e33b2f1f318e8
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-07-25 17:16:14 +0530

    |#11|bernie/priyanshu|added publish option on the editor

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,5 +1,35 @@
 let editor;
 
+const saveAndPublish = async function (title) {
+  const data = await editor.save();
+  out.innerHTML = JSON.stringify(Object.assign(data, { articleTitle: title }));
+};
+
+const publishBlog = function () {
+  const title = articleTitle.innerText;
+
+  if (!title) {
+    alert('A story without a title does not seem cool right...');
+  }
+
+  saveAndPublish(title);
+};
+
+const togglePublishedOnTitle = function () {
+  if (articleTitle.innerText.match(/\S/)) {
+    publishBtn.classList.remove('inactive');
+  } else {
+    publishBtn.classList.add('inactive');
+  }
+};
+
+const handleTitleKeypress = function () {
+  if (event.key === 'Enter') {
+    document.querySelector('#editorjs div[contenteditable=true]').focus();
+  }
+  togglePublishedOnTitle();
+};
+
 const createEditor = function () {
   editor = new EditorJS({
     holder: 'editorjs',
@@ -8,6 +38,9 @@ const createEditor = function () {
 
 const main = function () {
   createEditor();
+  articleTitle.addEventListener('keypress', handleTitleKeypress);
+  articleTitle.addEventListener('keyup', togglePublishedOnTitle);
+  publishBtn.addEventListener('click', publishBlog);
 };
 
 window.onload = main;
