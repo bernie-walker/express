c02923cf818a36806eb80d8840069b61770dd1fb
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-07-27 22:48:07 +0530

    |#13|bernie/shivam|added share feature

---
+++ public/js/blogPage.js
@@ -0,0 +1,28 @@
+const MESSAGE_APPEARANCE_TIME = 3000;
+
+const hideLinkCopiedMsg = function () {
+  linkCopiedMsg.classList.add('hidden');
+};
+
+const shareBlog = function () {
+  const textArea = document.createElement('textarea');
+  textArea.value = `localhost:3000/blogPage/${blogTitle.getAttribute(
+    'storyid'
+  )}`;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+  linkCopiedMsg.classList.remove('hidden');
+  setTimeout(hideLinkCopiedMsg, MESSAGE_APPEARANCE_TIME);
+};
+
+const main = function () {
+  logo.addEventListener('click', () => {
+    window.location = '/dashboard';
+  });
+
+  shareBtn.addEventListener('click', shareBlog);
+};
+
+window.onload = main;
