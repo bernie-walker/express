66f7b72b8c4de066a0a1e5d1eae01e27f4607234
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-07-28 12:04:41 +0530

    |#28|Thanya/Priyanshu| modified styling of heading
    added 'write a story' option with functionality

--- public/js/yourStories.js
+++ public/js/yourStories.js
@@ -1,13 +1,19 @@
 const showList = function (toShow, toHide) {
-  document.getElementById(toShow).classList.remove('hidden');
-  document.getElementById(toHide).classList.add('hidden');
+  const showingDiv = document.getElementById(toShow).classList.remove('hidden');
+  const hiddenDiv = document.getElementById(toHide).classList.add('hidden');
 };
 
 const attachOptionListeners = function () {
   Drafts.addEventListener('click', function () {
+    document.getElementById('Drafts').style['border-bottom'] =
+      '1.5px solid black';
+    document.getElementById('Published').style['border-bottom'] = '0';
     showList('DraftsList', 'PublishedList');
   });
   Published.addEventListener('click', function () {
+    document.getElementById('Drafts').style['border-bottom'] = '0';
+    document.getElementById('Published').style['border-bottom'] =
+      '1.5px solid black';
     showList('PublishedList', 'DraftsList');
   });
 };
