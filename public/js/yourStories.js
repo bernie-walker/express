2281c88dd29151f9672efbfa87f7a2ffcccdd5f0
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-07-28 12:47:44 +0530

    |#28|Thanya/Priyanshu| added link on each story card to land on blog page

--- public/js/yourStories.js
+++ public/js/yourStories.js
@@ -18,7 +18,18 @@ const attachOptionListeners = function () {
   });
 };
 
+const visitBlogPage = function (blogId) {
+  window.location = `/blogPage/${blogId}`;
+};
+
+const attachCardListeners = function () {
+  Array.from(document.querySelectorAll('.card'), (card) =>
+    card.addEventListener('click', visitBlogPage.bind(null, card.id))
+  );
+};
+
 window.onload = () => {
   attachHeadListener();
+  attachCardListeners();
   attachOptionListeners();
 };
